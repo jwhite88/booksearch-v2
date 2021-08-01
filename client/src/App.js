6f210aa56942ef4648cdb927d0394c76d40a1e85
jwhite88 <jwhite789@protonmail.com>
2021-08-01 00:08:33 -0400

    The first commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import AppNavbar from './components/AppNavbar';
+
+function App() {
+  return (
+    <Router>
+      <>
+        <AppNavbar />
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/saved' component={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
+  );
+}
+
+export default App;
